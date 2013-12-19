b032b487304d773bd882ce0ab3605dda56ba092f
Yann Kastelnik <yann@kastelnik.com>
2013-12-19 17:34:09 +0100

    Update assets configuration

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,7 @@ module PitchmeRailsBackbone
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     # Version of your assets, change this if you want to expire all your assets
