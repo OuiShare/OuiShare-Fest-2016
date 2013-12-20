f4f6e351ab404f0d89ca2f53d3d162c09e50ba7e
Fred-grais <frederic.grais@gmail.com>
2013-12-20 15:23:37 +0100

    merge with master

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
-module PitchmeRailsBackbone
+module OuiShareFest
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-PitchmeRailsBackbone::Application.configure do
+OuiShareFest::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
