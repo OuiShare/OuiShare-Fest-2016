8f86b1a801398fdb9d7ecbdd5c5d361f490d552b
Fred-grais <frederic.grais@gmail.com>
2014-01-06 14:52:12 +0100

    traductions system update

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -11,8 +11,53 @@ class Translation < ActiveRecord::Base
       
       if d['en']['dynamic_translations'].blank?
         d['en']['dynamic_translations'] = {}
-      end            
-      d['en']['dynamic_translations'][self.key] = self.value
+      end
+      splitted_key = key.split('.')
+      final_hash = []
+      older_hash = {}
+      new_hash = {}
+      count = splitted_key.length-1
+      splitted_key.length.times do           
+          if count == splitted_key.length-1
+            new_hash = Hash[splitted_key[count],self.value]           
+          else            
+            new_hash = Hash[splitted_key[count],older_hash]
+          end                 
+        older_hash = new_hash
+        count -= 1
+      end
+      final_hash = new_hash
+      
+      hash = {}
+      values_buffer = final_hash
+      final_hash.keys.each do |global_key|
+        if d["en"]["dynamic_translations"].has_key?(global_key)
+          d["en"]["dynamic_translations"][global_key].each do |key,value|            
+            
+            hash = hash.merge(key => value)
+            # final_hash = final_hash[global_key].merge(hash)
+            # final_hash_buffer = final_hash_buffer.merge(new_final_hash)
+            # new_final_hash = final_hash[global_key].merge(hash)
+            
+            
+            # final_hash_buffer = final_hash[global_key].merge(hash)
+            # d["en"]["dynamic_translations"][key].merge(final_hash[key])
+            # new_final_hash = Hash[global_key, final_hash_buffer]         
+          
+          end
+          
+          final_hash[global_key] = hash.merge(final_hash[global_key])
+         # final_hash[global_key] = final_hash[global_key].merge(hash)
+
+        end
+      end
+
+      
+      d["en"]["dynamic_translations"] = d["en"]["dynamic_translations"].merge(final_hash)
+      
+      # d["en"]["dynamic_translations"] = d["en"]["dynamic_translations"].merge(final_hash)
+      
+      # d['en']['dynamic_translations'][self.key] = self.value
       
       File.open(ENV['YAML_OUISHARE_FILE_PATH'], 'w') do |f|
         f.write d.to_yaml
