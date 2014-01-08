bc559f3ca0ad6e6df70da4ec215e71124209e7b2
Fred-grais <frederic.grais@gmail.com>
2014-01-08 09:47:51 +0100

    Translation system update

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -28,6 +28,8 @@ class Translation < ActiveRecord::Base
       end
       final_hash = new_hash
       
+      d['en']['dynamic_translations'][splitted_key[0]] = (Hash[d['en']['dynamic_translations'][splitted_key[0]].to_a + final_hash[splitted_key[0]].to_a])
+      
       hash = {}
       values_buffer = final_hash
       final_hash.keys.each do |global_key|
@@ -56,11 +58,35 @@ class Translation < ActiveRecord::Base
           d['en']['dynamic_translations'].delete self.key
       end 
     end
+    splitted_key = key.split('.')
+      final_hash = []
+      older_hash = {}
+      new_hash = {}
+      count = splitted_key.length-1
+      splitted_key.length.times do           
+          if count == splitted_key.length-1
+            new_hash = Hash[splitted_key[count],self.value]           
+          else            
+            new_hash = Hash[splitted_key[count],older_hash]
+          end                 
+        older_hash = new_hash
+        count -= 1
+      end
+      final_hash = new_hash
+      # value = nested_hash_value(splitted_key)
+
+          
+      
+      d['en']['dynamic_translations'][splitted_key[0]] = (Hash[d['en']['dynamic_translations'][splitted_key[0]].to_a - final_hash[splitted_key[0]].to_a])
+      if d['en']['dynamic_translations'][splitted_key[0]].blank?
+        d['en']['dynamic_translations'].delete splitted_key[0]
+      end
+      
     
     File.open(ENV['YAML_OUISHARE_FILE_PATH'], 'w') do |f|
         f.write d.to_yaml
     end
-  
+    
     connect_and_push_to_transifex()
   end
 
@@ -91,5 +117,42 @@ class Translation < ActiveRecord::Base
     end
     return request_response
   end
+
+  # def destroy_value_in_hash(initial_hash, splitted_key, occurences = 1, buffer_hash = nil, final_hash = nil)
+      
+      
+  #       if buffer_hash.blank? 
+  #         buffer_hash = initial_hash 
+  #         final_hash = {}
+  #         final_hash['en'] = {}
+  #         final_hash['en']['dynamic_translations'] = {}
+          
+  #       else
+  #         buffer_hash = buffer_hash
+  #       end     
+        
+  #       if buffer_hash.keys.include?(splitted_key[occurences])
+  #         if buffer_hash[splitted_key[occurences]].instance_of?(Hash)
+
+              
+  #           final_hash =final_hash['en']['dynamic_translations']
+                          
+            
+            
+  #           # initial_hash = Hash(initial_hash, buffer_hash[splitted_key[occurences]])
+  #           buffer_hash = buffer_hash[splitted_key[occurences]]
+  #           occurences +=1
+            
+  #           destroy_value_in_hash(initial_hash,splitted_key,occurences,buffer_hash, final_hash)
+  #         else
+            
+  #           final_hash = Hash[splitted_key[occurences-1],buffer_hash]
+  #           xx
+  #         end
+  #       end
+  #   return buffer_hash
+  # end
+
+  
 
 end
