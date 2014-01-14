475f3de179470805c350a378a66926c0f3b08b6e
Fred-grais <frederic.grais@gmail.com>
2014-01-14 12:32:32 +0100

    Transsifex error handling + css adjustments

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,7 +1,6 @@
 class Translation < ActiveRecord::Base
   attr_accessible :interpolations, :is_proc, :key, :locale, :value
 
-  # after_create :export_to_yaml
   after_save :export_to_yaml
   after_destroy :delete_from_yaml
 
@@ -104,17 +103,27 @@ class Translation < ActiveRecord::Base
     request_url = ENV['TRANSIFEX_OUISHARE_URL']
     uri = URI(base_url + request_url)
 
-    res = Net::HTTP.start(uri.host, 80) do |http|
-      request = Net::HTTP::Put.new uri.request_uri
-      request.basic_auth ENV['TRANSIFEX_CREDENTIAL_LOGIN'], ENV['TRANSIFEX_CREDENTIAL_SECRET']
-      request.content_type = "application/json"
-      request.body = file
-      
-      response = http.request request
-     
-      request_response = response.body
-      
-    end
+      begin
+        res = Net::HTTP.start(uri.host, 80) do |http|
+          request = Net::HTTP::Put.new uri.request_uri
+          request.basic_auth ENV['TRANSIFEX_CREDENTIAL_LOGIN'], ENV['TRANSIFEX_CREDENTIAL_SECRET']
+          request.content_type = "application/json"
+          request.body = file
+          
+          response = http.request request
+          
+          case response.code 
+            when "404"
+              request_response = "transifex repo not found"          
+            when "200"
+              request_response = response.body
+          end
+        
+        end
+      rescue
+        request_response = "An error occured on transifex"      
+      end
+
     return request_response
   end
 
