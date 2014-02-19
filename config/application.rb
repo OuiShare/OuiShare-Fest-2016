d01e250269f108a735f637d712fb531ffb6069a7
Yann Kastelnik <yann@kastelnik.com>
2014-02-19 15:10:21 +0100

    Activate spanish trads

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module OuiShareFest
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
-    config.i18n.available_locales = [:en, :fr]
+    config.i18n.available_locales = [:en, :fr, :es]
     config.i18n.fallbacks = true
     config.i18n.fallbacks = [:en]
 
