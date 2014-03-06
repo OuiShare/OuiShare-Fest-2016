a125a067ca17d546fbdd465a5c7028f771f1609e
karnaud <karol.arnaud@gmail.com>
2014-03-06 10:07:59 +0100

    pulling master

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -141,6 +141,7 @@ private
     eb_auth_tokens = { app_key: ENV['EVENTBRITE_APP_KEY'],
                    user_key: ENV['EVENTBRITE_USER_KEY']}
     eb_client = EventbriteClient.new(eb_auth_tokens)
+    
   end
 
   # def connect_to_sched
