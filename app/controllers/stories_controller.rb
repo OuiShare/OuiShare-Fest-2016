eb657b1626cb5323a6bf1954324f891af0c8df01
Yann Kastelnik <yann@kastelnik.com>
2014-05-27 22:50:29 +0200

    Add previous/next videos functions to adesias page, create stories controller and views for index & show story

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -8,5 +8,51 @@ class StoriesController < ApplicationController
 
   def show
     @adesias_video = Individual.find(params[:id])
+
+    @next_video = get_next_video
+    @previous_video = get_previous_video
+
+    @next_videos = get_next_videos(3)
   end
+
+  def get_next_videos(video_number)
+    @adesias_videos = IndividualType.find_by_title('adesias_videos').get_members
+    @next_videos = @adesias_videos.select{|n| n.id > params[:id].to_i}
+     
+    if !@next_videos.nil? && @next_videos.count >= video_number
+      @next_videos.first(video_number)
+    else
+      @adesias_videos.first(video_number - @next_videos.count).each do |video|
+        @next_videos << video
+      end
+      @next_videos.sort
+    end
+
+  end
+
+  def get_next_video
+    @adesias_videos = IndividualType.find_by_title('adesias_videos').get_members
+
+    @next_video = @adesias_videos.first(:conditions => ['individuals.id > ?', params[:id]], :order => 'id ASC')
+
+    if !@next_video.nil?
+      @next_video
+    else
+      @adesias_videos.first(:order => 'id ASC')
+    end
+
+  end 
+
+  def get_previous_video
+    @adesias_videos = IndividualType.find_by_title('adesias_videos').get_members
+
+    @previous_video = @adesias_videos.first(:conditions => ['individuals.id < ?', params[:id]], :order => 'id DESC')
+
+    if !@previous_video.nil?
+      @previous_video
+    else
+      @adesias_videos.first(:order => 'id DESC')
+    end
+  end
+
 end
