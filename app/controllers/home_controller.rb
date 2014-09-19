4ae51437602c48655041c2e8607cd469faea2b68
Karim Mortabit <karim.mortabit@gmail.com>
2014-09-19 12:07:38 +0200

    Change API credentials

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -198,7 +198,7 @@ private
   def connect_to_eventbrite
 
     eb_auth_tokens = { app_key: ENV['EVENTBRITE_APP_KEY'],
-                   user_key: ENV['EVENTBRITE_USER_KEY']}
+                   access_token: ENV['EVENTBRITE_USER_KEY']}
     eb_client = EventbriteClient.new(eb_auth_tokens)
 
   end
