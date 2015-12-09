5c7c321bb2457668f20bc86953071a7a1e613ca4
kirilind <code@haldjas.org>
2015-12-10 00:43:33 +0200

    launch2 e

--- config/routes.rb
+++ config/routes.rb
@@ -64,6 +64,7 @@ OuiShareFest::Application.routes.draw do
     get :entreprises
     get :press_and_media
     # get :fest2014
+    get :sliders
     get :travel_information
     get :where_to_stay
     get :getting_to_the_venue
