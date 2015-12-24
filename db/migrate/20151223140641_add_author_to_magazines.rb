918a01c340fac24328c986344908b8808893b79a
karnaud <karol.arnaud@gmail.com>
2015-12-24 09:44:54 +0100

    change migration for author

--- db/migrate/20151223140641_add_author_to_magazines.rb
+++ db/migrate/20151223140641_add_author_to_magazines.rb
@@ -1,5 +1,9 @@
 class AddAuthorToMagazines < ActiveRecord::Migration
-  def change
+  def up
   	add_column :magazines, :author, :string
+  end
+
+  def down
+  	remove_column :magazines, :author
   end
 end
